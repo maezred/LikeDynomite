feb02872825e1daca58389a844a5ec213317f2ae
Matthew Oltendorf <matthew@oltendorf.net>
2017-04-20 21:23:14 -0700

    Improve overriding of protection

--- src/main/kotlin/net/moltendorf/bukkit/likedynomite/Listeners.kt
+++ src/main/kotlin/net/moltendorf/bukkit/likedynomite/Listeners.kt
@@ -1,12 +1,18 @@
 package net.moltendorf.bukkit.likedynomite
 
 import org.bukkit.Material
+import org.bukkit.Material.FLINT_AND_STEEL
+import org.bukkit.Material.TNT
 import org.bukkit.block.Block
 import org.bukkit.entity.EntityType
 import org.bukkit.entity.TNTPrimed
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
+import org.bukkit.event.EventPriority.HIGHEST
+import org.bukkit.event.EventPriority.LOWEST
 import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockBreakEvent
+import org.bukkit.event.block.BlockPlaceEvent
 import org.bukkit.event.entity.EntityExplodeEvent
 import org.bukkit.event.hanging.HangingBreakEvent
 import org.bukkit.event.player.PlayerInteractEvent
@@ -21,21 +27,78 @@ import java.util.Collections
 class Listeners : Listener {
   private val chain = ExplosionChain()
 
+  @EventHandler(priority = LOWEST)
+  fun PlayerInteractEventLowestPriorityHandler(event: PlayerInteractEvent) {
+    if ((event.hasItem() && event.item.type == TNT) || (event.hasBlock() && event.clickedBlock.type == TNT)) {
+      // Pretend to cancel the event so that it isn't blocked by a protection plugin.
+      event.isCancelled = true
+
+      return
+    }
+  }
+
   @EventHandler(priority = EventPriority.HIGHEST)
-  fun PlayerInteractEventHandler(event: PlayerInteractEvent) {
-    val block = event.clickedBlock
+  fun PlayerInteractEventHighestPriorityHandler(event: PlayerInteractEvent) {
+    // Is the player placing TNT?
+    when (event.item?.type) {
+      TNT -> {
+        // We were just pretending.
+        event.isCancelled = false
 
-    // Did the player right-click TNT?
-    if (block != null && block.type == Material.TNT) {
-      val item = event.item
-
-      // Did they use Flint And Steel?
-      if (item != null && item.type == Material.FLINT_AND_STEEL) {
-        chain.add(block) // Queue it up.
-
-        // Cancel the event so another one isn't spawned (when no protection is present).
-        event.isCancelled = true
+        return
       }
+
+      FLINT_AND_STEEL -> { // Did the player ignite TNT?
+        if (event.hasBlock() && event.clickedBlock.type == TNT) {
+          chain.add(event.clickedBlock) // Queue it up.
+
+          // Cancel the event so another one isn't spawned.
+          event.isCancelled = true
+        }
+      }
+
+      else -> {
+        if (event.hasBlock() && event.clickedBlock.type == TNT) {
+          // We were just pretending.
+          event.isCancelled = false
+        }
+      }
+    }
+  }
+
+  @EventHandler(priority = LOWEST)
+  fun BlockPlaceEventLowestPriorityHandler(event: BlockPlaceEvent) {
+    // Is it TNT?
+    if (event.blockPlaced.type == TNT) {
+      // Pretend to cancel the event so that it isn't blocked by a protection plugin.
+      event.isCancelled = true
+    }
+  }
+
+  @EventHandler(priority = HIGHEST)
+  fun BlockPlaceEventHighestPriorityHandler(event: BlockPlaceEvent) {
+    // Is it TNT?
+    if (event.blockPlaced.type == TNT) {
+      // We were just pretending.
+      event.isCancelled = false
+    }
+  }
+
+  @EventHandler(priority = LOWEST)
+  fun BlockBreakEventLowestPriorityHandler(event: BlockBreakEvent) {
+    // Is it TNT?
+    if (event.block.type == TNT) {
+      // Pretend to cancel the event so that it isn't blocked by a protection plugin.
+      event.isCancelled = true
+    }
+  }
+
+  @EventHandler(priority = HIGHEST)
+  fun BlockBreakEventHighestPriorityHandler(event: BlockBreakEvent) {
+    // Is it TNT?
+    if (event.block.type == TNT) {
+      // We were just pretending.
+      event.isCancelled = false
     }
   }
 
